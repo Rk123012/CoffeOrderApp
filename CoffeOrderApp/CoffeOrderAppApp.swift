898589fb16f5b63b74f357dd6250daac125a19ca
Rezaul Karim <rk.jony@techetronventures.com>
2025-01-07 12:12:16 +0600

    Initial Commit

---
+++ CoffeOrderApp/CoffeOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeOrderAppApp.swift
+//  CoffeOrderApp
+//
+//  Created by Rezaul Karim on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
